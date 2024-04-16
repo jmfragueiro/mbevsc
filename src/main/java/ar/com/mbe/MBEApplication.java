0b0c8470e9c6007d57172ae8f4dc07b85542004b
git <jmfragueiro@hotmail.com>
2024-04-16 12:27:56 -0300

    desde el branch

--- src/main/java/ar/com/mbe/MBEApplication.java
+++ src/main/java/ar/com/mbe/MBEApplication.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class MBEApplication {
-    // punto de entrada a la app
+    // punto de entrada a la aplicacion
     public static void main(String[] args) {
         SpringApplication.run(MBEApplication.class, args);
     }
