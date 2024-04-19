df35ecdf91b3dae485151c9f165fb730fee7ef96
git <jmfragueiro@hotmail.com>
2024-04-19 13:16:20 -0300

    nueva prueba de git

--- src/main/java/ar/com/mbe/MBEApplication.java
+++ src/main/java/ar/com/mbe/MBEApplication.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class MBEApplication {
+    // punto de entrada a la aplicación
     public static void main(String[] args) {
         SpringApplication.run(MBEApplication.class, args);
     }
