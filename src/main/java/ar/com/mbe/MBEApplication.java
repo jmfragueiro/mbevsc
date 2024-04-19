486a9535a40aec9b4287cbc1aef909d27d1af51b
git <jmfragueiro@hotmail.com>
2024-04-19 13:34:50 -0300

    borrado de comentarios en punto de entrada

--- src/main/java/ar/com/mbe/MBEApplication.java
+++ src/main/java/ar/com/mbe/MBEApplication.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class MBEApplication {
-    // punto de entrada a la aplicación
     public static void main(String[] args) {
         SpringApplication.run(MBEApplication.class, args);
     }
